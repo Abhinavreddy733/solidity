1386c7096a6a80e65d5a0e0d30ab84f5d5b255fe
Abhinavreddy733 <abhinavreddy.k019@gmail.com>
2025-08-17 22:15:27 +0530

    will_contract

--- contracts/_Will.sol
+++ contracts/_Will.sol
@@ -1,7 +1,47 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
-contract Will {
 
-    uint256 number;
+contract Will {
+    address public owner;
+    address public recipient;
+    uint startTime;
+    uint lastVisited;
+    uint waitingYears;
+
+    constructor (address _recipient , uint _waitingYears) {
+        owner = msg.sender;
+        recipient = _recipient;
+        waitingYears = _waitingYears * 365 days;
+        startTime = block.timestamp;
+        lastVisited = block.timestamp;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can Habdle this");
+        _;
+    }
+
+    modifier onlyRecipient {
+        require(msg.sender == recipient, "Only recipient can Handle this");
+        _;
+    }
+
+    function Deposit () public payable onlyOwner {
+        lastVisited = block.timestamp;
+    }
+
+    function changeRecipient ( address newRecipient ) public onlyOwner {
+        recipient = newRecipient;
+    }
+
+    function ping() public onlyOwner {
+        lastVisited = block.timestamp;
+    }
+
+    function pullout() external onlyRecipient {
+        require(lastVisited < block.timestamp - waitingYears );
+        payable(recipient).transfer(address(this).balance);
+    }
+
 }
